8186d2e5a2cc5f3b4fa5d5535aed31bde54d6bef
Alptug Dilek <alptugd@gmail.com>
2019-12-19 00:35:12 +0300

    Perform remote call to github to fetch relevant repos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Paper, Typography } from "@material-ui/core"
 import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios'
 import SearchBar from './components/search-bar'
 
 const useStyles = makeStyles(theme => ({
@@ -9,16 +10,59 @@ const useStyles = makeStyles(theme => ({
         padding: theme.spacing(3, 2),
         textAlign: 'center'
     },
-}));
+}))
+
+const fetchRepos = async function(searchTerm) {
+    const result = await axios(
+        `https://api.github.com/search/repositories?q=${searchTerm}`
+    )
+
+    const
+        totalCount = result.data.total_count,
+        repos = result.data.items.map(item => {
+            return {
+                name: item.full_name,
+                avatarUrl: item.owner.avatar_url,
+                description: item.description,
+                stars: item.stargazers_count,
+                language: item.language
+            }
+        })
+
+    return { repos, totalCount }
+}
 
 function App() {
     const
         classes = useStyles(),
-        [ searchTerm, setSearchTerm ] = useState('')
+        [ searchTerm, setSearchTerm ] = useState(''),
+        [data, setData] = useState({
+            totalCount: undefined,
+            repos: []
+        }),
+        noSearchPerformedMessage = searchTerm ? null :
+            <Paper className={classes.root}>
+                <Typography component="p">
+                    Please submit searh term to see relevant repositories!
+                </Typography>
+            </Paper>,
+        dataInformationMessage = data.totalCount ? 
+            <Paper className={classes.root}>
+                <Typography component="p">
+                    A total of {data.totalCount} repos are found!
+                </Typography>
+            </Paper> : null
     
     useEffect(() => {
+        const fetchAndSetData = async () => {
+            const result = await fetchRepos(searchTerm)
+            setData(result)
+        }
+
         if (searchTerm) {
-            console.log(searchTerm)
+            fetchAndSetData()
+        } else {
+            setData({ totalCount: undefined, repos: [] })
         }
     }, [searchTerm])
 
@@ -27,11 +71,8 @@ function App() {
             <SearchBar 
                 onSearchTermChange={searchTerm => setSearchTerm(searchTerm)}
             />
-            <Paper className={classes.root}>
-                <Typography component="p">
-                    Please submit searh term to see relevant repositories!
-                </Typography>
-            </Paper>
+            { noSearchPerformedMessage }
+            { dataInformationMessage }
         </div>
     );
 }
