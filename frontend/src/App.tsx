f19c707d5268f7a8749c13b329276397e3720260
andrewkfiedler <andrewkfiedler@gmail.com>
2021-03-07 15:47:30 -0700

    Pull in a tailwind as a styling solution
     - Had to do a jump through a few hoops such as upgrading to the latest react scripts.  Make sure to clear out your node_modules for this one, I had to.
     - Two things, have to pull in a special version of tailwind that is postcss 7 compatible, and have to use craco to override cra slightly.
     - Never heard of craco before, but appears to be a nice way to modify cra without having to eject.
     - Verified that tailwind styles are able to be used.  Should verify that a production bundle of the app purges unused styles appropriately.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,14 +5,14 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="p-2">
         <header className="App-header">
           <h2>Welcome to the interview app!</h2>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.tsx</code> and save to reload.
           </p>
 
-        <ol>
+        <ol className="p-2">
           <li>Fetch Data from a public API <a href="https://github.com/toddmotto/public-apis">Samples</a></li>
           <li>Display data from API onto your page (Table, List, etc.)</li>
           <li>Apply a styling solution of your choice to make your page look different (CSS, SASS, CSS-in-JS)</li> 
