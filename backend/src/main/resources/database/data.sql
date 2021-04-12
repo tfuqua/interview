71ec53878b2935cd4786e382f7faeec7d8a1f546
Nodar Dzamukashvili <nodardzamukashvili@gmail.com>
2021-04-12 13:44:58 +0400

    added scripts and postman collection

--- backend/src/main/resources/database/data.sql
+++ backend/src/main/resources/database/data.sql
@@ -1,1 +1,10 @@
 -- Provide SQL scripts here
+CREATE TABLE CARS (
+   id IDENTITY PRIMARY KEY,
+   model VARCHAR(20) NOT NULL,
+   price DOUBLE
+);
+
+INSERT INTO CARS (id, model, price) VALUES (1, 'BMW', 145);
+INSERT INTO CARS (id, model, price) VALUES (2, 'Audi', 387.6);
+INSERT INTO CARS (id, model, price) VALUES (3, 'Toyota', 974);
